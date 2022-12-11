4fbea3826e068a64ac01cae0f5f37ed9e79df9e9
itsXRP <itsxrproms@gmail.com>
2022-12-11 07:48:20 +0000

    lunaa: Switch to aosp overlay
    Signed-off-by: itsXRP <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
