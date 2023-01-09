251c25fa9b079b8bba418f991ba2320c5c0d24b5
orges <me@orgesified.ml>
2023-01-09 22:59:31 +0100

    lunaa: configs: Add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Change-Id: I96331fb70c7f8c4bd6e630c9826465b232191fbe

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946741972277890.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946741972277890.xml
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
