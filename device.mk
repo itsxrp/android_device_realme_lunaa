7c0a3a36925ea0bdbcf62c59038a2bd169450117
basamaryan <basam.aryan@gmail.com>
2023-07-31 06:59:53 +0200

    lunaa: Use low gamma implementation for brightness curve
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_4630946741972277890.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946741972277890.xml
 
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.brightness.low.gamma=true
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
