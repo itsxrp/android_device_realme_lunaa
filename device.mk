5185b0aa8432a693a19a7684d3e84a46ab46bb7a
rk134 <rahul@aospa.co>
2023-09-22 07:40:36 +0000

    lunaa: init: Setup widevine certificate copying
    Signed-off-by: itsxrp <itsxrproms@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    init.oplus.widevine.rc
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
